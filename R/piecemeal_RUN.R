477d9d2d80332a94e124c2ce17478fe48edf723c
Aaron Chafetz <achafetz@usaid.gov>
2018-05-17 16:31:04 -0400

    update for IMxInd output

--- R/piecemeal_RUN.R
+++ R/piecemeal_RUN.R
@@ -10,7 +10,19 @@ library(fs)
 #import & combine
   df_targets <- map_dfr(.x = files, .f = ~read_xlsx(.x))
 
-#run
+#run OU output
   source("R/piecemeal_tidy.R")
   tidy(df_targets)
+  
+#setup IM output
+  source("R/piecemeal_setup.R")
+  setup(df_targets)
+  
+#break into pieces by IMs and indicators
+  
+  #TODO
+  x <- df %>% 
+    filter(mechanismuid == "18304",
+           indicator == "HTS_TST_POS") %>% 
+    spread(agesexother, fy2019_targets)
   
