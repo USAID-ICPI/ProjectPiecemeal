cb1bac9a4b04716f2ad9d1f96107dd01e7f4781d
Aaron Chafetz <achafetz@usaid.gov>
2018-05-17 08:58:46 -0400

    import using purrr

--- R/piecemeal_RUN.R
+++ R/piecemeal_RUN.R
@@ -1,14 +1,14 @@
 
+library(readxl)
+library(tidyverse)
+library(fs)
+
 
 #files
-  file <- dir_ls("RawData", glob = "*.xlsx")
+  files <- dir_ls("RawData", glob = "*.xlsx")
 
-  df_grp1 <- read_xlsx("RawData/USAID_sitexIM_FY19Targets_B-S_5.16.18.xlsx")
-  df_grp2 <- read_xlsx("RawData/USAID_sitexIM_FY19Targets_T-Z_5.16.18.xlsx")
-
-#combine
-  df_targets <- bind_rows(df_grp1, df_grp2)
-  rm(df_grp1, df_grp2)
+#import & combine
+  df_targets <- map_dfr(.x = files, .f = ~read_xlsx(.x))
 
 #run
   source("R/piecemeal_tidy.R")
