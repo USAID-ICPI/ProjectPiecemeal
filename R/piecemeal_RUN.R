8b2dbc6f2ed506607c60cc81f22308c0c7615200
Aaron Chafetz <achafetz@usaid.gov>
2018-05-18 16:21:58 -0400

    organize and remove tabulate to seperate fcn

--- R/piecemeal_RUN.R
+++ R/piecemeal_RUN.R
@@ -1,8 +1,13 @@
+##   Project Piecemeal
+##   A.Chafetz
+##   Purpose: breakout COP18 targets by OU and IMxIndicator
+##   Date: 2018.05.16
+##   Updated: 2018.05.18
 
-library(readxl)
-library(tidyverse)
-library(fs)
-
+#dependencies
+  library(readxl)
+  library(tidyverse)
+  library(fs)
 
 #files
   files <- dir_ls("RawData", glob = "*.xlsx")
@@ -10,35 +15,18 @@ library(fs)
 #import & combine
   df_targets <- map_dfr(.x = files, .f = ~read_xlsx(.x))
 
-#run OU output
+#run OU output (split tidy dataframes)
   source("R/piecemeal_tidy.R")
   tidy(df_targets)
   
 #setup IM output
   source("R/piecemeal_im_setup.R")
   df_targets <- setup(df_targets)
-  
-#break into pieces by IMs and indicators
-  mech_list <- unique(df_targets$mechanismuid)
-  
+
+#list of mechs and indicators to loop over
   df_full <- df_targets %>% 
     distinct(mechanismuid, indicator)
 
-  
-  #TODO
-  
-  ind_tabulate <- function(df, mech, ind){
-    df_mech <- df %>% 
-      filter(mechanismuid == !!mech,
-             indicator == !!ind) %>% 
-      spread(agesexother, fy2019_targets)
-    return(df_mech)
-  }
-  
-  
-  
-  df_targets %>% 
-    ind_tabulate( df_full$mechanismuid[1], df_full$indicator[1])
-    
- 
-  
+#generate output
+  source("R/piecemeal_tabulate.R")
+  map2(.x = df_full$mechanismuid, .y = df_full$indicator, .f = ~ tabulate(df_targets, .x, .y))
