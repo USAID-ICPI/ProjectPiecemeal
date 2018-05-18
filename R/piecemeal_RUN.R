aa3b7faff9dc99221c7097c60427e0b35c67e5e4
Aaron Chafetz <achafetz@usaid.gov>
2018-05-18 09:08:54 -0400

    add todo for purrr filter

--- R/piecemeal_RUN.R
+++ R/piecemeal_RUN.R
@@ -15,14 +15,30 @@ library(fs)
   tidy(df_targets)
   
 #setup IM output
-  source("R/piecemeal_setup.R")
-  setup(df_targets)
+  source("R/piecemeal_im_setup.R")
+  df_targets <- setup(df_targets)
   
 #break into pieces by IMs and indicators
+  mech_list <- unique(df_targets$mechanismuid)
+  
+  df_full <- df_targets %>% 
+    distinct(mechanismuid, indicator)
+
   
   #TODO
-  x <- df %>% 
-    filter(mechanismuid == "18304",
-           indicator == "HTS_TST_POS") %>% 
-    spread(agesexother, fy2019_targets)
+  
+  ind_tabulate <- function(df, mech, ind){
+    df_mech <- df %>% 
+      filter(mechanismuid == !!mech,
+             indicator == !!ind) %>% 
+      spread(agesexother, fy2019_targets)
+    return(df_mech)
+  }
+  
+  
+  
+  df_targets %>% 
+    ind_tabulate( df_full$mechanismuid[1], df_full$indicator[1])
+    
+ 
   
