fdf9ac080e47a3d695e79f57f55c2812958c917f
Aaron Chafetz <achafetz@usaid.gov>
2018-05-21 10:58:41 -0400

    add export and clean up

--- R/piecemeal_RUN.R
+++ R/piecemeal_RUN.R
@@ -4,29 +4,53 @@
 ##   Date: 2018.05.16
 ##   Updated: 2018.05.18
 
-#dependencies
+
+# DEPENDENCIES ------------------------------------------------------------
+
   library(readxl)
   library(tidyverse)
   library(fs)
 
-#files
-  files <- dir_ls("RawData", glob = "*.xlsx")
 
-#import & combine
-  df_targets <- map_dfr(.x = files, .f = ~read_xlsx(.x))
+# IMPORT & MUNGE ----------------------------------------------------------
 
-#run OU output (split tidy dataframes)
-  source("R/piecemeal_tidy.R")
-  tidy(df_targets)
+  #files
+    files <- dir_ls("RawData", glob = "*.xlsx")
   
-#setup IM output
-  source("R/piecemeal_im_setup.R")
-  df_targets <- setup(df_targets)
+  #import & combine
+    df_targets <- map_dfr(.x = files, .f = ~read_xlsx(.x))
+  
+  #clean up & format 
+    source("R/piecemeal_tidy.R")
+    df_targets <- tidy(df_targets)
 
-#list of mechs and indicators to loop over
-  df_full <- df_targets %>% 
-    distinct(mechanismuid, indicator)
 
-#generate output
-  source("R/piecemeal_tabulate.R")
-  map2(.x = df_full$mechanismuid, .y = df_full$indicator, .f = ~ tabulate(df_targets, .x, .y))
+# TEIR I - GENERIC OU FILES -----------------------------------------------
+
+  #identify all OUs in df
+    ou_list <- unique(df$operatingunit)
+    
+  #gen Output folder if it doesn't already exist
+    fs::dir_create("Output")
+  
+  #generate output for each export
+    source("R/piecemeal_ou_export.R")
+    purrr::map(.x = ou_list, .f = ~ ou_export(df_targets, .x))  
+  
+
+# TIER II - IM TABLES - PSNU X INDICATOR  ---------------------------------
+
+  #setup IM output
+    source("R/piecemeal_im_setup.R")
+    df_targets <- setup(df_targets)
+
+  #list of mechs and indicators to loop over
+    df_full <- df_targets %>% 
+      distinct(mechanismuid, indicator)
+
+  #generate output
+    source("R/piecemeal_tabulate.R")
+    map2(.x = df_full$mechanismuid, .y = df_full$indicator, .f = ~ tabulate(df_targets, .x, .y))
+  
+  #export
+    #TODO
