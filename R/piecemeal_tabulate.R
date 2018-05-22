8ec6343bb36c33c687a0f392b4fd428ab0d968d9
Aaron Chafetz <achafetz@usaid.gov>
2018-05-22 10:31:35 -0400

    use openxlsx to write to excel

--- R/piecemeal_tabulate.R
+++ R/piecemeal_tabulate.R
@@ -11,10 +11,21 @@ ind_tabulate <- function(df, mech, ind){
     spread(agesexother, fy2019_targets) %>% 
     rename(!!ind := psnu) #table name in upper left hand corner
   
-  return(df_mech)
-  
-  #TODO export to Excel
-    #use indicator as tab name
-    #save name COP18_targets_ou_mechid.xlsx
+  #export to Excel
+    #print to determine "location"
+    return(paste0(ou, ": ", mech, " - ", ind))
+    
+    #create file path to save  
+    path <- file.path("Output", paste0(ou, "_COP18-Targets_Mech-", mech, ".xlsx"))
+    
+    #if the file doesn't exist add it otherwise, open it
+    if(!dir.exists(path)){
+      wb <- createWorkbook()
+    } else{
+      wb <- loadWorkbook(path)
+    }
+    addWorksheet(wb, ind)
+    writeData(wb, ind, df_mech)
+    saveWorkbook(wb, path, overwrite = T)  
 
 }
