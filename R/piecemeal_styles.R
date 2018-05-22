b53a39c29e6defbb11407da26192ad435afe2b62
Aaron Chafetz <achafetz@usaid.gov>
2018-05-22 12:27:41 -0400

    add styles

---
+++ R/piecemeal_styles.R
@@ -0,0 +1,16 @@
+
+#table styles
+
+  style_headers <- createStyle(textDecoration = "bold", wrapText = TRUE,
+                               halign = "center", valign = "center",
+                               border = "Bottom")
+  
+  style_title <- createStyle(textDecoration = "bold", fontSize = 14,
+                             valign = "Top", border = "BottomRight")
+  
+  style_psnus <- createStyle(border = "Right") 
+  
+  style_values <- createStyle(numFmt = "#,##0")
+  
+  
+  
