faed3aa04b8c2daacfd00adc9cb57031b0f73edb
Aaron Chafetz <achafetz@usaid.gov>
2018-05-22 11:05:20 -0400

    fix bug, dir.exists -> file.exists

--- R/piecemeal_tabulate.R
+++ R/piecemeal_tabulate.R
@@ -12,20 +12,19 @@ ind_tabulate <- function(df, mech, ind){
     rename(!!ind := psnu) #table name in upper left hand corner
   
   #export to Excel
-    #print to determine "location"
-    return(paste0(ou, ": ", mech, " - ", ind))
-    
     #create file path to save  
     path <- file.path("Output", paste0(ou, "_COP18-Targets_Mech-", mech, ".xlsx"))
     
     #if the file doesn't exist add it otherwise, open it
-    if(!dir.exists(path)){
+    if(!file.exists(path)){
       wb <- createWorkbook()
     } else{
       wb <- loadWorkbook(path)
     }
     addWorksheet(wb, ind)
     writeData(wb, ind, df_mech)
-    saveWorkbook(wb, path, overwrite = T)  
-
+    saveWorkbook(wb, path, overwrite = T)
+    
+    #print to determine "location"
+    paste0(ou, ": ", mech, " - ", ind)
 }
