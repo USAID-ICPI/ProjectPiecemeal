8e9637e374003f378b455ef52f9772064c9ef83c
Aaron Chafetz <achafetz@usaid.gov>
2018-05-22 12:27:59 -0400

    add table formatting

--- R/piecemeal_tabulate.R
+++ R/piecemeal_tabulate.R
@@ -13,17 +13,41 @@ ind_tabulate <- function(df, mech, ind){
   
   #export to Excel
     #create file path to save  
-    path <- file.path("Output", paste0(ou, "_COP18-Targets_Mech-", mech, ".xlsx"))
+      path <- file.path("Output", paste0(ou, "_COP18-Targets_Mech-", mech, ".xlsx"))
     
     #if the file doesn't exist add it otherwise, open it
-    if(!file.exists(path)){
-      wb <- createWorkbook()
-    } else{
-      wb <- loadWorkbook(path)
-    }
-    addWorksheet(wb, ind)
-    writeData(wb, ind, df_mech)
-    saveWorkbook(wb, path, overwrite = T)
+      if(!file.exists(path)){
+        wb <- createWorkbook()
+      } else{
+        wb <- loadWorkbook(path)
+      }
+    #create worksheet off indicator name
+      addWorksheet(wb, ind)
+    #write data
+      writeData(wb, ind, df_mech)
+    
+    #identify table dimensions for formatting
+      n_col <- ncol(df_mech)
+      n_row <- as.integer(nrow(df_mech) + 1)
+      m <- as.integer(max(n_col, n_row))
+    
+    #apply styles
+      #indicator/sheet title
+      addStyle(wb, ind, style_title, rows = 1, cols = 1)
+      #headers  
+      addStyle(wb, ind, style_headers, rows = 1, cols = 2:n_col)
+      #psnus
+      addStyle(wb, ind, style_psnus, rows = 2:n_row, cols = 1)
+      #values
+      addStyle(wb, ind, style_values, rows = 2:m, cols = 2:m, gridExpand = TRUE)
+      
+    #adjust width/height  
+      #increase the width for the psnu column
+      setColWidths(wb, ind, cols=1, widths = 20)
+      #increase the row height for the headers
+      setRowHeights(wb, ind, rows = 1, heights = 30)
+    #save
+      saveWorkbook(wb, path, overwrite = T)
     
     #print to determine "location"
     paste0(ou, ": ", mech, " - ", ind)
