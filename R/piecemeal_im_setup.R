7ce22c5594bd4836dfe83481a985dd92a7df6c8d
Aaron Chafetz <achafetz@usaid.gov>
2018-05-22 13:50:47 -0400

    add missing disaggs

--- R/piecemeal_im_setup.R
+++ R/piecemeal_im_setup.R
@@ -63,12 +63,13 @@ setup <- function(df){
     summarise_at(vars(fy2019_targets), ~ sum(., na.rm = TRUE)) %>% 
     ungroup()
   
-  ordered <- c("Unknown Sex <1", "<= 2 months", "Male < 2 months", "2 - 12 months", "Male 2 months - 9 years", "Unknown Sex 1-9", 
+  ordered <- c("Unknown Sex <1", "<= 2 months", "Male < 2 months", "2 - 12 months", "Male 2 months - 9 years", "Unknown Sex 1-9", "Female 1-9", 
                "Unknown Sex <5","Female 10-14", "Male 10-14", "Female <15", "Male <15", "Female 15+", "Male 15+", "Female 15-19", 
                "Male 15-19", "Female 15-17", "Male 15-17", "Female 18-24", "Male 18-24", "Female 20-24", "Male 20-24", "Female 25+", 
                "Male 25+", "Female 25-29", "Male 25-29", "Female 30-34", "Male 30-34", "Female 35-39", "Male 35-39", "Female 40-49", 
-               "Male 40-49", "Female 50+", "Male 50+", "AlreadyPregnant", "NewPregnant", "KnownPregnant", "NewPregnant :  Negative", 
-               "NewPregnant :  Positive", "FSW", "MSM not SW", "TG not SW", "People in prisons and other enclosed settings", 
+               "Male 40-49", "Female 50+", "Male 50+", "Female Unknown Age", "Male Unknown Age", "AlreadyPregnant", "NewPregnant", 
+               "KnownPregnant", "NewPregnant :  Negative", "NewPregnant :  Positive", "FSW", "MSM not SW", "MSM SW", 
+               "People in prisons and other enclosed settings", "PWID", "TG not SW", "TG SW", 
                "Physical and/or Emotional Violence", "Sexual Violence (Post-Rape Care)", "Total Numerator")
   
   df <- df %>% 
